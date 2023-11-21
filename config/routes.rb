7444b05890a5a4cad3e5012c68f9b56280d4c766
kiran aziz <kiranaziz637@gmail.com>
2023-11-21 15:22:32 +0000

    challengedone

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  delete "tasks/:id", to: "tasks#destroy"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: "task"
 end
